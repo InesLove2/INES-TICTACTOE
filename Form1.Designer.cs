990d8cdcbe6f730514ee48008bb21fa4d4cdfa04
InesLove2 <inesmegouya12@gmail.com>
2024-05-28 18:50:49 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Casino_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbl_casino = new System.Windows.Forms.Label();
+            this.lbl_balance = new System.Windows.Forms.Label();
+            this.lbl_win = new System.Windows.Forms.Label();
+            this.lbl_loss = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_bid = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Casino_Game.Properties.Resources.img_9;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Casino_Game.Properties.Resources.img_9;
+            this.pictureBox2.Location = new System.Drawing.Point(251, 144);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(86, 134);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Casino_Game.Properties.Resources.img_9;
+            this.pictureBox3.Location = new System.Drawing.Point(548, 144);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(86, 134);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lbl_casino
+            // 
+            this.lbl_casino.AutoSize = true;
+            this.lbl_casino.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_casino.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_casino.Location = new System.Drawing.Point(12, 29);
+            this.lbl_casino.Name = "lbl_casino";
+            this.lbl_casino.Size = new System.Drawing.Size(110, 32);
+            this.lbl_casino.TabIndex = 3;
+            this.lbl_casino.Text = "Casino";
+            // 
+            // lbl_balance
+            // 
+            this.lbl_balance.AutoSize = true;
+            this.lbl_balance.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_balance.ForeColor = System.Drawing.Color.IndianRed;
+            this.lbl_balance.Location = new System.Drawing.Point(526, 29);
+            this.lbl_balance.Name = "lbl_balance";
+            this.lbl_balance.Size = new System.Drawing.Size(160, 32);
+            this.lbl_balance.TabIndex = 4;
+            this.lbl_balance.Text = "Balance: $";
+            // 
+            // lbl_win
+            // 
+            this.lbl_win.AutoSize = true;
+            this.lbl_win.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_win.ForeColor = System.Drawing.Color.Magenta;
+            this.lbl_win.Location = new System.Drawing.Point(-3, 346);
+            this.lbl_win.Name = "lbl_win";
+            this.lbl_win.Size = new System.Drawing.Size(116, 32);
+            this.lbl_win.TabIndex = 5;
+            this.lbl_win.Text = "Wins: 0";
+            // 
+            // lbl_loss
+            // 
+            this.lbl_loss.AutoSize = true;
+            this.lbl_loss.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_loss.ForeColor = System.Drawing.Color.Red;
+            this.lbl_loss.Location = new System.Drawing.Point(-3, 424);
+            this.lbl_loss.Name = "lbl_loss";
+            this.lbl_loss.Size = new System.Drawing.Size(113, 32);
+            this.lbl_loss.TabIndex = 6;
+            this.lbl_loss.Text = "Loss: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(527, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bid -Amount";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(520, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 38);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_play.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_play.Location = new System.Drawing.Point(0, 563);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(185, 85);
+            this.btn_play.TabIndex = 9;
+            this.btn_play.Text = "play";
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.Click += new System.EventHandler(this.btn_player_Click);
+            // 
+            // btn_bid
+            // 
+            this.btn_bid.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_bid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bid.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_bid.Location = new System.Drawing.Point(520, 563);
+            this.btn_bid.Name = "btn_bid";
+            this.btn_bid.Size = new System.Drawing.Size(185, 85);
+            this.btn_bid.TabIndex = 10;
+            this.btn_bid.Text = "New Bid";
+            this.btn_bid.UseVisualStyleBackColor = false;
+            this.btn_bid.Click += new System.EventHandler(this.btn_bid_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Casino_Game.Properties.Resources.img2;
+            this.ClientSize = new System.Drawing.Size(761, 660);
+            this.Controls.Add(this.btn_bid);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_loss);
+            this.Controls.Add(this.lbl_win);
+            this.Controls.Add(this.lbl_balance);
+            this.Controls.Add(this.lbl_casino);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Casino Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbl_casino;
+        private System.Windows.Forms.Label lbl_balance;
+        private System.Windows.Forms.Label lbl_win;
+        private System.Windows.Forms.Label lbl_loss;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_bid;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casino_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int a, b, c, move, wins, balance , loss;
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_bid_Click(object sender, EventArgs e)
+        {
+            lbl_casino.Text = "Casino" ;
+            balance = 0;
+            textBox1.Enabled = true;
+            textBox1.Text = "";
+            textBox1.BackColor = Color.White;
+            textBox1.Focus();
+            btn_play.Enabled = true;
+            btn_play.Text = "play";
+            lbl_balance.Text = "Balance : $";
+
+        }
+
+        void Game_Result()
+        {
+            if (System.Convert.ToInt32(a&b)!=c)
+            {
+                wins++;
+                lbl_win.Text = "wins" + wins;
+                balance += 2;
+                lbl_balance.Text = "Balance : $" + Convert.ToInt32(textBox1.Text) * balance;
+                btn_play.Text = "continue playing..";
+                lbl_casino.Text = "you won";
+            }
+
+            else
+            {
+                lbl_casino.Text = "Try again";
+                loss++;
+                lbl_loss.Text = "loss :" + loss;
+                lbl_balance.Text = "Balance : $ 0";
+                balance = 0;
+                textBox1.Text = "";
+                btn_play.Enabled = false ;
+                pictureBox1.Image = Properties.Resources.img_9;
+                pictureBox2.Image = Properties.Resources.img_9;
+                pictureBox3.Image = Properties.Resources.img_9;
+            }
+
+        }
+
+        private void btn_player_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("please enter the bid amount inside the textbox to play..");
+            }
+            else
+            {
+                timer1.Enabled = true;
+                lbl_casino.Text = "Casino";
+                textBox1.Enabled = false;
+                textBox1.BackColor = Color.Black;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move++;
+            if (move < 30)
+            {
+                a = rnd.Next(5);
+                b = rnd.Next(5);
+                c = rnd.Next(5);
+                switch (a)
+                {
+
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.img4;
+                        break;
+
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.img8;
+                        break;
+
+                    case 3:
+                        pictureBox1.Image = Properties.Resources.img5;
+                        break;
+
+                }
+
+                switch (b)
+                {
+
+                    case 1:
+                        pictureBox2.Image = Properties.Resources.img4;
+                        break;
+
+                    case 2:
+                        pictureBox2.Image = Properties.Resources.img8;
+                        break;
+
+                    case 3:
+                        pictureBox2.Image = Properties.Resources.img5;
+                        break;
+
+                }
+                switch (c)
+                {
+
+                    case 1:
+                        pictureBox3.Image = Properties.Resources.img4;
+                        break;
+
+                    case 2:
+                        pictureBox3.Image = Properties.Resources.img8;
+                        break;
+
+                    case 3:
+                        pictureBox3.Image = Properties.Resources.img5;
+                        break;
+
+                }
+            }
+            else
+            {
+                timer1.Enabled = false;
+                move = 0;
+                Game_Result();
+
+            }
+
+            }
+        }
+    }
